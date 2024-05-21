0239ad147d5888fae80247c057ae574507d4dbbf
krvn-0 <kasamson@up.edu.ph>
2024-05-22 03:05:35 +0800

    styling: style donor home page

--- lib/pages/donorhomepage/donorhomepage.dart
+++ lib/pages/donorhomepage/donorhomepage.dart
@@ -50,117 +50,35 @@ class _DonorHomePageState extends State<DonorHomePage> {
     super.dispose();
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     backgroundColor: const Color(0xFFEEF2E6),
-  //     appBar: AppBar(
-  //       title: const Text("Donor's View",
-  //           style: TextStyle(color: Color(0xFFEEF2E6), fontWeight: FontWeight.bold)),
-  //       backgroundColor: const Color(0xFF093731),
-  //       iconTheme: const IconThemeData(color: Color(0xFFEEF2E6)),
-  //     ),
-  //     body: Column(
-  //       children: [
-  //         Container(
-  //           decoration: const BoxDecoration(
-  //               color: Color(0xFF093731),
-  //               borderRadius: BorderRadius.only(
-  //                   bottomLeft: Radius.circular(25),
-  //                   bottomRight: Radius.circular(25))),
-  //           height: 200,
-  //           padding: const EdgeInsets.symmetric(
-  //             vertical: 50.0,
-  //           ),
-  //           width: double.infinity,
-  //           child: const Column(
-  //             children: [
-  //               Center(
-  //                   child: Text("Donate Now!",
-  //                       style: TextStyle(
-  //                           color: Color(0xFFEEF2E6),
-  //                           fontSize: 20,
-  //                           fontWeight: FontWeight.bold))),
-  //               SizedBox(height: 20),
-  //               Center(
-  //                 child: Text("Choose an organization below",
-  //                     style: TextStyle(
-  //                         fontSize: 20,
-  //                         fontWeight: FontWeight.bold,
-  //                         color: Color(0xFFEEF2E6))),
-  //               )
-  //             ],
-  //           ),
-  //         ),
-  //         Padding(
-  //           padding: const EdgeInsets.all(16.0),
-  //           child: SearchBar(
-  //             controller: _searchController,
-  //             backgroundColor: MaterialStateProperty.all(const Color(0xFFD6CDA4)),
-  //             leading: const Icon(Icons.search, color: Color(0xFF093731)),
-  //             hintText: "Seach organization...",
-  //             hintStyle: MaterialStateProperty.all(const TextStyle(
-  //               color: Color(0xFF093731)
-  //             )),
-  //           ),
-  //         ),
-  //         Expanded(
-  //           child: ListView.builder(
-  //             padding: const EdgeInsets.all(8),
-  //             itemCount: filteredOrganizations.length,
-  //             itemBuilder: (BuildContext context, int index) {
-  //               return Card(
-  //                 clipBehavior: Clip.hardEdge,
-  //                 child: InkWell(
-  //                   splashColor: const Color(0xFF3D8361).withAlpha(100),
-  //                   onTap: () {
-  //                     debugPrint(filteredOrganizations[index]);
-  //                     Navigator.pushNamed(context, "/donor/donate");
-  //                   },
-  //                   child: SizedBox(
-  //                     width: double.infinity,
-  //                     height: 150,
-  //                     child: Padding(
-  //                       padding: const EdgeInsets.all(8.0),
-  //                       child: Center(
-  //                         child: Text(
-  //                           filteredOrganizations[index],
-  //                           style: const TextStyle(
-  //                               fontSize: 15, fontWeight: FontWeight.bold, color: Color(0xFF093731)),
-  //                         ),
-  //                       ),
-  //                     ),
-  //                   ),
-  //                 ),
-  //               );
-  //             },
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const Placeholder(),
       backgroundColor: const Color(0xFFEEF2E6),
       body: CustomScrollView(
         slivers: [
           const SliverAppBar(
-            leading: Icon(Icons.menu),
-            title: Text("Donate Now!"),
             // automaticallyImplyLeading: false,
-            backgroundColor: const Color(0xFF093731),
+            iconTheme: IconThemeData(color: Color(0xFFEEF2E6)),
+            backgroundColor: Color(0xFF093731),
             pinned: true,
             expandedHeight: 300,
             flexibleSpace: FlexibleSpaceBar(
-              expandedTitleScale: 1.5,
-              // centerTitle: true,
-              title: Text("Donate Now!", style: TextStyle(color: Color(0xFFEEF2E6), fontWeight: FontWeight.bold)),
+               titlePadding: EdgeInsets.only(left: 0, bottom: 16), // REMOVE PADDING SO THAT CENTER IS NOT OFFSET
+              centerTitle: true,
+              title: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("Donate Now!", style: TextStyle(color: Color(0xFFEEF2E6), fontWeight: FontWeight.bold)),
+                  Text(
+                    "Scroll to explore organizations",
+                    style: TextStyle(color: Color(0xFFEEF2E6), fontSize: 12),
+                  ),
+                ],
+              ),
             ),
           ),
-          SliverAppBar( // TODO use SliverPersistentHeader instead
+          SliverAppBar( // TODO use SliverPersistentHeader instead pag masipag ka xdxd
             backgroundColor: const Color(0xFFEEF2E6),
             automaticallyImplyLeading: false,
             pinned: true,
@@ -180,26 +98,29 @@ class _DonorHomePageState extends State<DonorHomePage> {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
-                return Card(
-                  clipBehavior: Clip.hardEdge,
-                  child: InkWell(
-                    splashColor: const Color(0xFF3D8361).withAlpha(100),
-                    onTap: () {
-                      debugPrint(filteredOrganizations[index]);
-                      Navigator.pushNamed(context, "/donor/donate");
-                    },
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 150,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(
-                          child: Text(
-                            filteredOrganizations[index],
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF093731),
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Card(
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: const Color(0xFF3D8361).withAlpha(100),
+                      onTap: () {
+                        debugPrint(filteredOrganizations[index]);
+                        Navigator.pushNamed(context, "/donor/donate");
+                      },
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 150,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Text(
+                              filteredOrganizations[index],
+                              style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF093731),
+                              ),
                             ),
                           ),
                         ),
