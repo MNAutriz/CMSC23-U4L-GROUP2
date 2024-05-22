a0ae20d90d668d71fb0aca491dbf3e28d26167ed
krvn-0 <kasamson@up.edu.ph>
2024-05-22 14:56:44 +0800

    ui: use stack to add bg image to org cards

--- lib/pages/donorhomepage/donorhomepage.dart
+++ lib/pages/donorhomepage/donorhomepage.dart
@@ -17,6 +17,10 @@ class DonorHomePage extends StatefulWidget {
 
 class _DonorHomePageState extends State<DonorHomePage> {
   int _selectedIndex = 0;
+
+  String sampleImage =
+      'https://adra.ph/wp-content/uploads/2017/09/Gift-Boxes-Aeta-4-1024x683.jpg';
+
   TextEditingController _searchController = TextEditingController();
   String searchQuery = '';
 
@@ -54,7 +58,13 @@ class _DonorHomePageState extends State<DonorHomePage> {
               title: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Text("Donate Now!", style: TextStyle(color: Color(0xFFEEF2E6), fontWeight: FontWeight.bold)),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8.0),
+                    child: Text("Donate Now!",
+                        style: TextStyle(
+                            color: Color(0xFFEEF2E6),
+                            fontWeight: FontWeight.bold)),
+                  ),
                   Text(
                     "Scroll to explore organizations",
                     style: TextStyle(color: Color(0xFFEEF2E6), fontSize: 12),
@@ -71,7 +81,8 @@ class _DonorHomePageState extends State<DonorHomePage> {
               padding: const EdgeInsets.all(16.0),
               child: SearchBar(
                 controller: _searchController,
-                backgroundColor: MaterialStateProperty.all(const Color(0xFFD6CDA4)),
+                backgroundColor:
+                    MaterialStateProperty.all(const Color(0xFFD6CDA4)),
                 leading: const Icon(Icons.search, color: Color(0xFF093731)),
                 hintText: "Search organization...",
                 hintStyle: MaterialStateProperty.all(const TextStyle(
@@ -98,7 +109,8 @@ class _DonorHomePageState extends State<DonorHomePage> {
   }
 
   Widget stream() {
-    Stream<QuerySnapshot> orgsStream = context.watch<OrganizationProvider>().organization;
+    Stream<QuerySnapshot> orgsStream =
+        context.watch<OrganizationProvider>().organization;
 
     return StreamBuilder(
       stream: orgsStream,
@@ -131,39 +143,74 @@ class _DonorHomePageState extends State<DonorHomePage> {
         return SliverList(
           delegate: SliverChildBuilderDelegate(
             (context, index) {
-              Organization org = Organization.fromJson(docs[index].data() as Map<String, dynamic>);
+              Organization org = Organization.fromJson(
+                  docs[index].data() as Map<String, dynamic>);
               org.id = docs[index].id;
 
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Card(
-                  clipBehavior: Clip.hardEdge,
-                  child: InkWell(
-                    splashColor: const Color(0xFF3D8361).withAlpha(100),
-                    onTap: () {
-                      debugPrint(org.name);
-                      Navigator.pushNamed(context, "/donor/donatedrives");
-                    },
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 150,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(
-                          child: Text(
-                            org.name,
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF093731),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+              return Stack(children: [
+                // Padding(
+                //   padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                //   child: Card(
+                //     clipBehavior: Clip.hardEdge,
+                //     child: InkWell(
+                //       splashColor: const Color(0xFF3D8361).withAlpha(100),
+                //       onTap: () {
+                //         debugPrint(org.name);
+                //         Navigator.pushNamed(context, "/donor/donatedrives");
+                //       },
+                //       child: Image.network(sampleImage),
+                // child: SizedBox(
+                //   width: double.infinity,
+                //   height: 150,
+                //   child: Padding(
+                //     padding: const EdgeInsets.all(8.0),
+                //     child: Center(
+                //       child: Text(
+                //         org.name,
+                //         style: const TextStyle(
+                //           fontSize: 15,
+                //           fontWeight: FontWeight.bold,
+                //           color: Color(0xFF093731),
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                //     ),
+                //   ),
+                // ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: Card(
+                        clipBehavior: Clip.hardEdge,
+                        child: InkWell(
+                            child: Image.network(sampleImage, fit: BoxFit.cover),
+                            onTap: () {
+                              Navigator.pushNamed(context, '/donor/donatedrives');
+                            })),
                   ),
                 ),
-              );
+                Positioned.fill(
+                  // so that text will fill the entire card and the center widget will center according to entire card
+                  child: Center(
+                    child: Text(org.name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Color(0xFFEEF2E6),
+                            shadows: [
+                              Shadow(
+                                blurRadius: 10,
+                                color: Colors.black,
+                                offset: Offset(2, 2)
+                              )
+                            ])),
+                  ),
+                )
+              ]);
             },
             childCount: docs.length,
           ),
@@ -188,34 +235,32 @@ class _DonorHomePageState extends State<DonorHomePage> {
   }
 
   Drawer get drawer => Drawer(
-    child: Container(
-      color: const Color(0xFFEEF2E6),
-      child: ListView(padding: EdgeInsets.zero, children: [
-        const DrawerHeader(
-          decoration: BoxDecoration(color: Color(0xFF1C6758)),
-          child: Text("Settings",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              color: Color(0xFFEEF2E6))
-          )
-        ),
-        ListTile(
-          title: const Center(
-            child: Text(
-              "Log out",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Color(0xFF1C6758)),
+        child: Container(
+          color: const Color(0xFFEEF2E6),
+          child: ListView(padding: EdgeInsets.zero, children: [
+            const DrawerHeader(
+                decoration: BoxDecoration(color: Color(0xFF1C6758)),
+                child: Text("Settings",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30,
+                        color: Color(0xFFEEF2E6)))),
+            ListTile(
+              title: const Center(
+                child: Text(
+                  "Log out",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Color(0xFF1C6758)),
+                ),
+              ),
+              onTap: () {
+                context.read<UserAuthProvider>().signOut();
+              },
             ),
-          ),
-          onTap: () {
-            context.read<UserAuthProvider>().signOut();
-          },
+          ]),
         ),
-      ]),
-    ),
-  );
+      );
 }
