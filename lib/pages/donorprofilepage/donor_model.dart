64d63217c6128f5be7494fdbae36a855924150d8
krvn-0 <kasamson@up.edu.ph>
2024-05-22 11:54:24 +0800

    feat: get email from firebase auth

--- lib/pages/donorprofilepage/donor_model.dart
+++
@@ -1,8 +0,0 @@
-class Donor {
-  String? firstname;
-  String? lastname;
-  String? email;
-  String? contactno;
-
-  Donor({required this.firstname, required this.lastname, required this.email, required this.contactno});
-}
