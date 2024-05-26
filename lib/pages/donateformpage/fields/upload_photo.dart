b5acbd80291a00938bc830aa7ccbcead84ab8463
krvn-0 <kasamson@up.edu.ph>
2024-05-26 16:23:06 +0800

    feat: upload from camera/gallery now functional

--- lib/pages/donateformpage/fields/upload_photo.dart
+++ lib/pages/donateformpage/fields/upload_photo.dart
@@ -1,32 +1,72 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UploadPhotoButtons extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UploadPhotoButtons extends StatefulWidget {
   const UploadPhotoButtons({super.key});
 
   @override
+  State<UploadPhotoButtons> createState() => _UploadPhotoButtonsState();
+}
+
+class _UploadPhotoButtonsState extends State<UploadPhotoButtons> {
+
+  File ?_selectedImage;
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: IconButton(
-                icon: const Icon(Icons.attach_file, color: Color(0xFF3D8361)),
-                onPressed: () {},
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  child: IconButton(
+                    icon: const Icon(Icons.attach_file, color: Color(0xFF3D8361)),
+                    onPressed: () {
+                      pickImageFromGallery();
+                    },
+                  )
+                )
+                ),
+              ),
+            Expanded(
+              child: Card(
+                child: IconButton(
+                  icon: const Icon(Icons.camera_alt, color: Color(0xFF3D8361),),
+                  onPressed: () {
+                    pickImageFromCamera();
+                  }
+                )
               )
-            )
             ),
-          ),
-        Expanded(
-          child: Card(
-            child: IconButton(
-              icon: const Icon(Icons.camera_alt, color: Color(0xFF3D8361),),
-              onPressed: () {}
-            )
-          )
+          ],
         ),
+        _selectedImage != null ? Image.file(_selectedImage!) : Container(),
       ],
     );
+  }
+
+  Future pickImageFromGallery() async  {
+    final returnedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    if(returnedImage == null) return;
+
+    setState(() {
+      _selectedImage = File(returnedImage.path);
+    });
+  }
+
+  Future pickImageFromCamera() async {
+    final returnedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+
+    if(returnedImage == null) return;
+
+    setState(() {
+      _selectedImage = File(returnedImage.path);
+    });
   }
 }
