697e316f16b1477daaeff5f6696ca9c399a7b1b8
MNAutriz <mgautriz@up.edu.ph>
2024-05-27 12:07:03 +0800

    Added the CRUD operations, created individual pages for donations

--- Desktop/cmsc_23/CMSC23-U4L-GROUP2/lib/pages/donationdrivepage/donation_drive_details.dart
+++ Desktop/cmsc_23/CMSC23-U4L-GROUP2/lib/pages/donationdrivepage/donation_drive_details.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
+import 'donation_drive_model.dart';
+import '../donationpage/donation_page.dart';
 
 class DonationDriveDetails extends StatelessWidget {
-  final String title;
-  final String description;
-  final List<String> imageUrls;
-  final double raisedAmount;
-  final double goalAmount;
+  final DonationDrive donationDrive;
 
-  DonationDriveDetails({
-    required this.title,
-    required this.description,
-    required this.imageUrls,
-    required this.raisedAmount,
-    required this.goalAmount,
-  });
+  DonationDriveDetails({required this.donationDrive});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(donationDrive.title),
         backgroundColor: Color(0xFF093731),
       ),
       backgroundColor: Color(0xFFF7F7F7),
@@ -30,22 +22,13 @@ class DonationDriveDetails extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                title,
+                donationDrive.title,
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 10),
               Text(
-                description,
+                donationDrive.description,
                 style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(height: 20),
-              Text(
-                '\$${raisedAmount.toStringAsFixed(2)} raised of \$${goalAmount.toStringAsFixed(2)} goal',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.green,
-                  fontWeight: FontWeight.bold,
-                ),
               ),
               SizedBox(height: 20),
               Text(
@@ -61,11 +44,29 @@ class DonationDriveDetails extends StatelessWidget {
                   crossAxisSpacing: 10,
                   mainAxisSpacing: 10,
                 ),
-                itemCount: imageUrls.length,
+                itemCount: donationDrive.imageUrls.length,
                 itemBuilder: (context, index) {
                   return Image.network(
-                    imageUrls[index],
+                    donationDrive.imageUrls[index],
                     fit: BoxFit.cover,
+                  );
+                },
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Linked Donations:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: donationDrive.donations.length,
+                itemBuilder: (context, index) {
+                  final donation = donationDrive.donations[index];
+                  return ListTile(
+                    title: Text(donation.title),
+                    subtitle: Text(donation.description),
+                    trailing: Text('\$${donation.amountRaised.toStringAsFixed(2)} raised'),
                   );
                 },
               ),
