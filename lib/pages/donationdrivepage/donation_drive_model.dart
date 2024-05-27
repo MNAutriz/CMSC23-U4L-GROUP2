6b3ccbbc03ffc0a1b88355ae702103c6e9812bb4
MNAutriz <mgautriz@up.edu.ph>
2024-05-27 22:58:48 +0800

    Added the providers and updated the models

--- lib/pages/donationdrivepage/donation_drive_model.dart
+++ lib/pages/donationdrivepage/donation_drive_model.dart
@@ -1,11 +1,11 @@
 import '../homepage/donation_model.dart';
 
 class DonationDrive {
-  final String id;
-  final String title;
-  final String description;
-  final List<String> imageUrls;
-  final List<Donation> donations;
+  String id;
+  String title;
+  String description;
+  List<String> imageUrls;
+  List<Donation> donations;
 
   DonationDrive({
     required this.id,
@@ -14,4 +14,26 @@ class DonationDrive {
     required this.imageUrls,
     required this.donations,
   });
+
+  // Convert a DonationDrive into a Map.
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'imageUrls': imageUrls,
+      'donations': donations.map((donation) => donation.toJson()).toList(),
+    };
+  }
+
+  // Convert a Map into a DonationDrive.
+  factory DonationDrive.fromJson(Map<String, dynamic> json) {
+    return DonationDrive(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      imageUrls: List<String>.from(json['imageUrls']),
+      donations: List<Donation>.from(json['donations'].map((donation) => Donation.fromJson(donation))),
+    );
+  }
 }
--- lib/pages/homepage/donation_model.dart
+++ lib/pages/homepage/donation_model.dart
@@ -16,4 +16,30 @@ class Donation {
     this.driveDetails,
     this.status = 'Pending',
   });
+
+  // Convert a Donation into a Map.
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'description': description,
+      'imageUrl': imageUrl,
+      'amountRaised': amountRaised,
+      'goal': goal,
+      'driveDetails': driveDetails,
+      'status': status,
+    };
+  }
+
+  // Convert a Map into a Donation.
+  factory Donation.fromJson(Map<String, dynamic> json) {
+    return Donation(
+      title: json['title'],
+      description: json['description'],
+      imageUrl: json['imageUrl'],
+      amountRaised: json['amountRaised'],
+      goal: json['goal'],
+      driveDetails: json['driveDetails'],
+      status: json['status'],
+    );
+  }
 }
