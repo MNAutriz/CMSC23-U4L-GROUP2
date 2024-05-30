ae6ed42e8b5fcb2ec81773a0ba5302fe20a28621
krvn-0 <kasamson@up.edu.ph>
2024-05-31 00:57:32 +0800

    styling: style buttons in donation drives page

--- lib/pages/donationdrivepage/donation_drive_card.dart
+++ lib/pages/donationdrivepage/donation_drive_card.dart
@@ -1,7 +1,8 @@
 import '../../providers/donation_drive_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:cmsc23project/providers/donation_drive_provider.dart' as provider;
+import 'package:cmsc23project/providers/donation_drive_provider.dart'
+    as provider;
 import '../donationdrivepage/donation_drive_model.dart';
 import 'edit_donation_drive_page.dart';
 import 'donation_drive_details.dart';
@@ -16,6 +17,7 @@ class DonationDriveCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.hardEdge,
       margin: const EdgeInsets.all(10),
       elevation: 4,
       color: Colors.white, // Set the background color to white
@@ -23,7 +25,9 @@ class DonationDriveCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Image.network(
-            donationDrive.coverPhoto.isNotEmpty ? donationDrive.coverPhoto : 'https://via.placeholder.com/150',
+            donationDrive.coverPhoto.isNotEmpty
+                ? donationDrive.coverPhoto
+                : 'https://via.placeholder.com/150',
             height: 200,
             fit: BoxFit.cover,
           ),
@@ -34,7 +38,8 @@ class DonationDriveCard extends StatelessWidget {
               children: [
                 Text(
                   donationDrive.title,
-                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 5),
                 Text(
@@ -43,37 +48,140 @@ class DonationDriveCard extends StatelessWidget {
                 ),
                 const SizedBox(height: 10),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DonationDriveDetailsPage(donationDrive: donationDrive, orgEmail: orgEmail,),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DonationDriveDetailsPage(
+                                donationDrive: donationDrive,
+                                orgEmail: orgEmail,
+                              ),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: const Color(0xFFEEF2E6),
+                          backgroundColor:
+                              const Color(0xFF093731), // text color
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(16), // rounded corners
                           ),
-                        );
-                      },
-                      child: const Text('See details'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => EditDonationDrivePage(donationDrive: donationDrive),
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 16), // padding
+                              // minimumSize: const Size(double.infinity, 0)
+                        ),
+                        child: const Text(
+                          "See Details",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
                           ),
-                        );
-                      },
-                      child: const Text('Edit'),
+                        ),
+                      ),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Provider.of<DonationDriveProvider>(context, listen: false)
-                            .deleteDonationDrive(donationDrive.id);
-                      },
-                      child: const Text('Delete'),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => EditDonationDrivePage(
+                                    donationDrive: donationDrive),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: const Color(0xFFEEF2E6),
+                            backgroundColor:
+                                const Color(0xFF093731), // text color
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(16), // rounded corners
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 16), // padding
+                          ),
+                          child: const Text(
+                            "Edit",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Provider.of<DonationDriveProvider>(context,
+                                    listen: false)
+                                .deleteDonationDrive(donationDrive.id);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: const Color(0xFFEEF2E6),
+                            backgroundColor: Colors.red, // text color
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(16), // rounded corners
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 16), // padding
+                          ),
+                          child: const Text(
+                            "Delete",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //         builder: (context) => DonationDriveDetailsPage(
+                    //           donationDrive: donationDrive,
+                    //           orgEmail: orgEmail,
+                    //         ),
+                    //       ),
+                    //     );
+                    //   },
+                    //   child: const Text('See details'),
+                    // ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Navigator.push(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //         builder: (context) => EditDonationDrivePage(
+                    //             donationDrive: donationDrive),
+                    //       ),
+                    //     );
+                    //   },
+                    //   child: const Text('Edit'),
+                    // ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     Provider.of<DonationDriveProvider>(context,
+                    //             listen: false)
+                    //         .deleteDonationDrive(donationDrive.id);
+                    //   },
+                    //   child: const Text('Delete'),
+                    // ),
                   ],
                 ),
               ],
